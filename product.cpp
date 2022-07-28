5612d5abe068e0f3dc0badf7e27ee760367737db
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-07-28 23:00:24 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,28 @@
+//function that returns the product of two numbers entered by user.
+
+#include <iostream>
+
+using namespace std;
+
+int product(int x,int y){
+return x*y;
+}
+
+int main(){
+
+int num1;
+int num2;
+
+cout<<"first number";
+cin>>num1;
+
+cout<<"second number";
+cin>>num2;
+
+cout<<"product of two is " << product(num1,num2);
+return 0;
+}
+
+
+
+//code by ankushsrj Jul 28 @23:00
