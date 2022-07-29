ad3e36f3dbf57b91501ff33affe629769d515b83
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-07-29 13:43:38 +0530

    Create substr.cpp

---
+++ substr.cpp
@@ -0,0 +1,14 @@
+//to find substring in an string, use substr()
+#include<iostream>
+ #include<string>
+
+ using namespace std;
+
+  int main (){
+  string s1= "AnkushSinghIsMyName";
+  string s = s1.substr(6,5);
+
+cout<<s<<endl;
+  return 0;
+
+  }
