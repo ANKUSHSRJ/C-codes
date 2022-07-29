a0e6a7ecf93b1a9bc874a4bd8982f81fa5583972
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-07-29 08:57:22 +0530

    Create clearnemptystring.cpp

---
+++ clearnemptystring.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+ #include<string>
+
+ using namespace std;
+
+  int main (){
+  string s1= "Ankush";
+ s1.clear();
+  if(s1.empty()){
+  cout<<"String is empty";
+  }
+  if(!s1.empty()){
+  cout<<"String is not empty";
+  }
+
+  return 0;
+
+  }
