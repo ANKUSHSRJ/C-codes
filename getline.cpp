2c2887101ed0c560594dbbf68b38ffe28a6f56fe
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-07-29 08:26:55 +0530

    Create getline.cpp

---
+++ getline.cpp
@@ -0,0 +1,16 @@
+// using get line
+ #include <iostream>
+ #include<string>
+ using namespace std;
+int main (){
+
+string str;
+getline(cin,str);
+cout<<"Hey check it - "<<str<<endl;
+
+return 0;
+
+}
+
+
+
