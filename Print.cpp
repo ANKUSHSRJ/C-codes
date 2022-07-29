5b1179602d27831c09fe56979d55df7a3f403ca2
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-07-29 06:14:27 +0530

    Create Print.cpp

---
+++ Print.cpp
@@ -0,0 +1,18 @@
+//SImply print whta you enter 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int age;
+
+cout<<"What is your age";
+cin>>age;
+cout<<"Your Current age is - "<<age;
+
+return 0;
+}
+
+
+
