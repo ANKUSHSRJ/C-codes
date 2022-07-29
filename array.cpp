36741f309663fe33f691076c7c476dce842a4889
Ankush SRJ <69152440+ANKUSHSRJ@users.noreply.github.com>
2022-07-29 07:09:38 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,17 @@
+//Simply print an array 
+
+ #include <iostream>
+ using namespace std;
+
+ int main (){
+  int arr[5]= {1,2,3,4};
+
+  for (int i =0; i<5; i++){
+    cout<<arr[i]<<" ";
+  }
+   return 0;
+
+ }
+
+
+
